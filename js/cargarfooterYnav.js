0fbf2901066fa4e957eda00e3a2014f1600af60d
Isaac684 <hc21018@ues.edu.sv>
2023-06-12 15:41:32 -0600

    Mejora del blog principal

--- js/cargarfooterYnav.js
+++ js/cargarfooterYnav.js
@@ -1,18 +1,19 @@
 //Codigo html del nav
 const nav = `
 <nav class="navbar navbar-expand-lg navbar-dark ">
-    <div class="container-fluid colorPrincipal">
-        <a class="navbar-brand fs-3" href="#">Conociendo El Salvador</a>
+    <div class="container-fluid d-flex colorPrincipal">
+        <a class="navbar-brand fs-3 " href="#">Conociendo El Salvador</a>
+        
         <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
             <div class="offcanvas-header colorPrincipal">
-                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Realizar busqueda:</h5>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
-            <div class="offcanvas-body colorPrincipal">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <div class="offcanvas-body d-flex flex-lg-row flex-md-column colorPrincipal">
+                <ul class="mt-lg-0 mt-md-3 navbar-nav justify-content-lg-end align-items-start flex-grow-1 pe-3">
                     <li class="nav-item">
                         <a class="nav-link active " aria-current="page" href="#">Home</a>
                     </li>
@@ -23,7 +24,7 @@ const nav = `
                         <a class="nav-link" aria-current="page" href="#">Acerca de</a>
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
+                <form class="d-flex order-md-first order-lg-last" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-success" type="submit">Search</button>
                 </form>
@@ -31,7 +32,7 @@ const nav = `
         </div>
         <div class="dropdown ms-3">
             <a class="text-light dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="bi bi-person-circle fs-2"></i>
+                <i class="bi bi-person-circle text-white fs-2"></i>
             </a>
             <ul class="dropdown-menu dropdown-menu-end">
                 <li>
