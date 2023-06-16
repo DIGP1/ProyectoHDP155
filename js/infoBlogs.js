b1acdacc65efc7202e7eee274ea40016b58e1e5f
DIGP1 <Diegogarci098@gmail.com>
2023-06-16 11:55:18 -0600

    Se a creado la funcion de guardar blog

---
+++ js/infoBlogs.js
@@ -0,0 +1,40 @@
+let bannerSubido = document.getElementById('banner');
+let direcbanner; 
+bannerSubido.addEventListener('change', function(event) {
+let banner = event.target.files[0]; 
+    direcbanner = banner;
+    let reader = new FileReader();
+      
+    reader.onload = function(e) {
+    let verBanner = document.getElementById('verBanner');
+    verBanner.src = e.target.result; 
+    direcbanner = e.target.result;
+    verBanner.style.display = 'block';
+    verBanner.style.width = "30%";
+    verBanner.style.height = "10rem";
+
+    }
+      
+
+reader.readAsDataURL(banner);
+});
+
+const btnG = document.querySelector("#btnGuardar");
+moment.locale("es");
+btnG.addEventListener("click", (e) => {
+    e.preventDefault()
+    titulo = document.getElementById("titulo").value;
+    cuerpo = document.getElementById("cuerpo").value;
+    const fechaBlog =moment().format('DD/MM/YYYY HH:mm:ss');
+
+    blogs = JSON.parse(localStorage.getItem("blogs")) || [];
+    blogs.push({
+        titulo: titulo,
+        cuerpo: cuerpo,
+        banner: direcbanner,
+        fecha: fechaBlog
+    });
+    localStorage.setItem("blogs", JSON.stringify(blogs));
+    alert("¡Se ha guardado el blog con éxito!");
+});
+  
