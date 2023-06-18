ed4ce0bfc0335c97a36a1210b9b27615088df465
DIGP1 <Diegogarci098@gmail.com>
2023-06-18 11:43:10 -0600

    Correccion a errores

--- js/administrador.js
+++ js/administrador.js
@@ -22,10 +22,11 @@
 //mostrando los datos e eliminar
 
 const adminUser = document.querySelector('[data-users]');
-const userTableContainer = document.getElementById('userTableContainer');
+const info= document.getElementById('contenedor');
 
 adminUser.addEventListener('click', (e) => {
    e.preventDefault();
+   info.innerHTML ="";
    const users = JSON.parse(localStorage.getItem('users')) || [];
 
    const table = document.createElement('table');
@@ -59,8 +60,8 @@ adminUser.addEventListener('click', (e) => {
       </tbody>
    `;
 
-   userTableContainer.innerHTML = '';
-   userTableContainer.appendChild(table);
+   info.innerHTML = '';
+   info.appendChild(table);
 
    const check = document.querySelectorAll('input[type="checkbox"]');
    const borrar = document.querySelectorAll('.btn-delete');
@@ -128,15 +129,13 @@ const comentarios = document.querySelector('[data-comentarios]')
 
 adminPost.addEventListener('click', (e) => {
    e.preventDefault();
- 
    const blogs = JSON.parse(localStorage.getItem('blogs')) || [];
-   const userTableContainer = document.getElementById('userTableContainer');
-   userTableContainer.innerHTML = '';
+   info.innerHTML = '';
  
    if (blogs.length === 0) {
       const messageDiv = document.createElement('div');
       messageDiv.innerHTML = `<div class="alert alert-secondary">No hay blogs disponibles.</div>`;
-      userTableContainer.appendChild(messageDiv);
+      info.appendChild(messageDiv);
    } else {
       const cardsHTML = blogs.map(blog => `
        <div class="col-md-4 mb-4">
@@ -159,7 +158,7 @@ adminPost.addEventListener('click', (e) => {
      rowDiv.classList.add('row');
      rowDiv.innerHTML = cardsHTML;
  
-     userTableContainer.appendChild(rowDiv);
+     info.appendChild(rowDiv);
    }
 });
 
@@ -169,77 +168,87 @@ adminPost.addEventListener('click', (e) => {
 
 
 
-const info= document.getElementById('contenedor')
 
 
-comentarios.addEventListener('click',()=>{
+
+const mostrar = () => {
    info.innerHTML = "";
-   let comentariosBlog = JSON.parse(localStorage.getItem('comentarios')) || [];
-   if(comentariosBlog.length != 0){
+ 
+   const comentariosBlog = JSON.parse(localStorage.getItem('comentarios')) || [];
+ 
+   if (comentariosBlog.length !== 0) {
      ordenarPorFecha(comentariosBlog);
-     let bandera = null
-     for (const j of comentariosBlog) {
-      if(!j.mostrar){
-         bandera = 1
-         const fechaComen = moment(j.fecha, "DD/MM/YYYY HH:mm:ss");
+     let comentariosMostrados = false;
+ 
+     for (const [index, comentario] of comentariosBlog.entries()) {
+       if (!comentario.mostrar) {
+         const fechaComen = moment(comentario.fecha, "DD/MM/YYYY HH:mm:ss");
          const fecharelComentario = fechaComen.fromNow();
          let comentarioMostrado = `
-         <div class="container-sm border border-2 rounded mt-3 mb-1">
-         <div class="fs-6 text-dark">${fecharelComentario}</div>
-         <div class="row">
-            <div class="col-auto">
-               <i class="bi bi-person-circle text-dark fs-3 "></i>
-            </div>
-            <div class="col-auto d-flex align-items-center">
-            <div class="fs-5 text-dark">${j.user}</div>
-            </div>
-         </div>
-           <div class="row">
-             <div class="col-6">
-               <span class="text-wrap fs-4 text-dark">${j.cuerpo}</span>
+           <div class="container-sm border border-2 rounded mt-3 mb-1">
+             <div class="fs-6 text-dark">${fecharelComentario}</div>
+             <div class="row">
+               <div class="col-auto">
+                 <i class="bi bi-person-circle text-dark fs-3 "></i>
+               </div>
+               <div class="col-auto d-flex align-items-center">
+                 <div class="fs-5 text-dark">${comentario.user}</div>
+               </div>
              </div>
-             <div class="col-4 d-flex align-items-center">
-             <button class="btn btn-success mb-1" btnAceptar>Mostrar</button>
-             <button class="btn btn-danger mb-1" btnQuitar>Quitar</button>
+             <div class="row">
+               <div class="col-6">
+                 <span class="text-wrap fs-4 text-dark">${comentario.cuerpo}</span>
+               </div>
+               <div class="col-4 d-flex align-items-center">
+                 <button class="btn btn-success mb-1 btnAceptar" data-index="${index}">Mostrar</button>
+                 <button class="btn btn-danger mb-1 btnQuitar" data-index="${index}">Quitar</button>
+               </div>
              </div>
            </div>
-         </div>
          `;
          info.innerHTML += comentarioMostrado;
-         //Eventos de aceptar comentario o eliminarlo
-         let btnAceptar = document.querySelectorAll("[btnAceptar]");
-         let btnEliminar = document.querySelectorAll("[btnQuitar]");
-
-         for (const l of btnAceptar) {
-            l.addEventListener("click",()=>{
-               info.innerHTML ="";
-               j.mostrar = true;
-               localStorage.setItem('comentarios', JSON.stringify(comentariosBlog));
-               if(!j.mostrar){
-                  comentariosBlog = JSON.parse(localStorage.getItem('comentarios')) || [];
-                  info.innerHTML += comentarioMostrado;
-               }
-               
-            });
-         }
-         
-      }else if(bandera == null){
-         info.innerHTML = `
-         <div class="badge bg-secondary text-wrap fs-5" style="width: 100%;">
-            No tienes comentarios a revisar
-         </div>
-         `;
-      }
+         comentariosMostrados = true;
+       }
      }
-     
-   }else{
-      info.innerHTML = `
-      <div class="badge bg-secondary text-wrap fs-5" style="width: 100%;">
+ 
+     if (!comentariosMostrados) {
+       info.innerHTML = `
+         <div class="badge bg-secondary text-wrap fs-5" style="width: 100%;">
+           No tienes comentarios a revisar
+         </div>
+       `;
+     }
+   } else {
+     info.innerHTML = `
+       <div class="badge bg-secondary text-wrap fs-5" style="width: 100%;">
          Aun no hay comentarios en los post!
-      </div>
-      `;
+       </div>
+     `;
    }
-})
+ 
+   const btnAceptarList = document.getElementsByClassName('btnAceptar');
+   const btnQuitarList = document.getElementsByClassName('btnQuitar');
+ 
+   for (let i = 0; i < btnAceptarList.length; i++) {
+     btnAceptarList[i].addEventListener('click', () => {
+       const index = btnAceptarList[i].dataset.index;
+       comentariosBlog[index].mostrar = true;
+       localStorage.setItem('comentarios', JSON.stringify(comentariosBlog));
+       mostrar();
+     });
+   }
+ 
+   for (let i = 0; i < btnQuitarList.length; i++) {
+     btnQuitarList[i].addEventListener('click', () => {
+       const index = btnQuitarList[i].dataset.index;
+       comentariosBlog.splice(index, 1);
+       localStorage.setItem('comentarios', JSON.stringify(comentariosBlog));
+       mostrar();
+     });
+   }
+ };
+ 
+ comentarios.addEventListener('click', mostrar);
 
 function ordenarPorFecha(arreglo){
    arreglo.sort((a, b) => {
