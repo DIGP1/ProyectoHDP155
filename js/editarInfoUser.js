0438185c29ab51ccbbc41117ef7614e389af2160
DIGP1 <Diegogarci098@gmail.com>
2023-06-18 23:01:41 -0600

    Arreglo de ver mi perfil

--- js/editarInfoUser.js
+++ js/editarInfoUser.js
@@ -1,80 +1,31 @@
+const infousers = JSON.parse(localStorage.getItem('users')) || [];
+const datauser = JSON.parse(localStorage.getItem("login_success"));
 
-// export const editar = () => {
-//     const usuarios = JSON.parse(localStorage.getItem('login_success')) || {};
-  
-   
-//     const emailInput = document.getElementById("correoelectronico");
-//     const nameInput = document.getElementById("nombrecompleto");
-//     const passwordInput = document.getElementById("contraseña");
-//     const usuarioInput = document.getElementById("usuarioo");
-  
-  
-//     usuarios.email = emailInput;
-//     usuarios.name = nameInput;
-//     usuarios.password = passwordInput;
-//     usuarios.user = usuarioInput;
-  
-   
-//     localStorage.setItem('login_success', JSON.stringify(usuarios));
-  
-// };
+const validarusuario = infousers.find(user => (user.user === datauser.user ));
 
-// export default document.getElementById('editarbtn').addEventListener('click', editar);
-const editar = () => {
-    const usuarios = JSON.parse(localStorage.getItem('users')) || [];
-    const userLogin = JSON.parse(localStorage.getItem('login_success')) || {};
-  
-    const usuarioIndex = usuarios.findIndex(
-      usuario =>
-        usuario.usuario === userLogin.usuario &&
-        usuario.email === userLogin.email &&
-        usuario.password === userLogin.password &&
-        usuario.name === userLogin.name
-    );
-  
-    /* La condición if (usuarioIndex !== -1) verifica si se encontró el usuario en la lista. Si el valor de usuarioIndex es 
-    diferente de -1, significa que se encontró el usuario y se procede a realizar la edición. Si el valor de usuarioIndex es -1, 
-    indica que el usuario no se encontró en la lista y se muestra un mensaje de alerta correspondiente. */
-    if (usuarioIndex !== -1) {
-      if (usuarios[usuarioIndex].password !== document.getElementById("contraseña").value) {
-        let passwd = prompt('Para confirmar los cambios, introduce tu contraseña antigua:');
-        if (passwd === usuarios[usuarioIndex].password) {
-          usuarios[usuarioIndex] = {
-            ...usuarios[usuarioIndex],
-            email: document.getElementById("correoelectronico").value,
-            name: document.getElementById("nombrecompleto").value,
-            password: document.getElementById("contraseña").value,
-            user: document.getElementById("usuarioo").value
-          };
-          localStorage.setItem('users', JSON.stringify(usuarios));
-          localStorage.setItem('login_success', JSON.stringify(usuarios[usuarioIndex]));
-  
-          alert('Información de usuario actualizada correctamente.');
-        }else {
-          alert('Contraseña incorrecta, los cambios no serán aplicados.');
-        }
-      }else {
-        let passwd = prompt('Para confirmar los cambios, introduce tu contraseña:');
-        if (passwd === usuarios[usuarioIndex].password) {
-          usuarios[usuarioIndex] = {
-            ...usuarios[usuarioIndex],
-            email: document.getElementById("correoelectronico").value,
-            name: document.getElementById("nombrecompleto").value,
-            password: document.getElementById("contraseña").value,
-            user: document.getElementById("usuarioo").value
-          };
-          localStorage.setItem('users', JSON.stringify(usuarios));
-          localStorage.setItem('login_success', JSON.stringify(usuarios[usuarioIndex]));
-  
-          alert('Información de usuario actualizada correctamente.');
-        }else {
-          alert('Contraseña incorrecta, los cambios no serán aplicados.');
-        }
-      }
-      
-    } else {
-      alert('No se encontró el usuario en la lista.');
+
+const nombre = document.getElementById("nombrecompleto");
+const usuarioo = document.getElementById("usuarioo");
+const correo = document.getElementById("correoelectronico");
+const contra = document.getElementById("contraseña");
+
+nombre.value = validarusuario.name;
+usuarioo.value = validarusuario.user;
+correo.value = validarusuario.email;
+contra.value = validarusuario.password;
+
+const btnEditar = document.getElementById("editarbtn");
+
+btnEditar.addEventListener("click", ()=>{
+  for (const i of infousers) {
+    if (validarusuario.user == i.user && validarusuario.password == validarusuario.password) {
+      i.name = nombre.value;
+      i.user = usuarioo.value;
+      i.email = correo.value;
+      i.password = contra.value;
+
+      localStorage.setItem("users",JSON.stringify(infousers));
+      localStorage.setItem("login_success",JSON.stringify(i));
     }
-  };
-  
-document.getElementById('editarbtn').addEventListener('click', editar);
+  }
+});
